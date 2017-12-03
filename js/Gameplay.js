f76537970ff105c294ad72a3746a43ad444391b1
Minimuino <minimuino@gmail.com>
2017-12-03 19:39:28 +0100

    New stuff

--- js/Gameplay.js
+++ js/Gameplay.js
@@ -3,7 +3,9 @@ Game.Gameplay = function(game)
 	this.pointer;
 	this.girl;
 	this.walk_tween;
-	this.growth_interval = 5;
+	this.growth_interval = 3;
+	this.sprites_to_grow;
+	this.crits;
 	this.sounds = [];
 };
 
@@ -11,10 +13,13 @@ Game.Gameplay.prototype =
 {
 	createGirl: function()
 	{
-		this.girl = this.add.sprite(200, 90, 'body');
+		this.girl = this.add.sprite(200, 85, 'body');
 
 		// Body sprites
-		this.girl.addChild(this.make.sprite(0, 0, 'head'));
+		var head = this.make.sprite(0, 0, 'head', 0);
+		var head_anim = head.animations.add('wind', [0, 1], 5, true);
+		head_anim.play('wind');
+		this.girl.addChild(head);
 		this.girl.getChildAt(0).addChild(this.make.sprite(0, 0, 'face'));
 		this.girl.inputEnabled = true;
 		// Hair sprites
@@ -22,14 +27,40 @@ Game.Gameplay.prototype =
 		arm_left.inputEnabled = true;
 		arm_left.events.onInputDown.add(this.removeHair, this);
 		this.girl.addChild(arm_left);
+		var arm_right = this.make.sprite(327, 371, 'arm_right', 0);
+		arm_right.inputEnabled = true;
+		arm_right.events.onInputDown.add(this.removeHair, this);
+		this.girl.addChild(arm_right);
+		var armpit_left = this.make.sprite(117, 211, 'armpit_left', 0);
+		armpit_left.inputEnabled = true;
+		armpit_left.events.onInputDown.add(this.removeHair, this);
+		this.girl.addChild(armpit_left);
+		var armpit_right = this.make.sprite(304, 239, 'armpit_right', 0);
+		armpit_right.inputEnabled = true;
+		armpit_right.events.onInputDown.add(this.removeHair, this);
+		this.girl.addChild(armpit_right);
+		var eyebrow = this.make.sprite(217, 76, 'eyebrow', 0);
+		eyebrow.inputEnabled = true;
+		eyebrow.events.onInputDown.add(this.removeHair, this);
+		this.girl.addChild(eyebrow);
+		var moustache = this.make.sprite(210, 122, 'moustache', 0);
+		moustache.inputEnabled = true;
+		moustache.events.onInputDown.add(this.removeHair, this);
+		this.girl.addChild(moustache);
+		var pubis = this.make.sprite(191, 429, 'pubis', 0);
+		pubis.inputEnabled = true;
+		pubis.events.onInputDown.add(this.removeHair, this);
+		this.girl.addChild(pubis);
+
+		this.sprites_to_grow = [1, 2, 3, 4, 5, 6, 7];
 
 		// Move down tween
-		var body_down_1 = this.add.tween(this.girl).to( { y: '+5' }, 500, Phaser.Easing.Cubic.Out);
-		var body_down_2 = this.add.tween(this.girl).to( { y: '+10' }, 500, Phaser.Easing.Cubic.Out);
+		var body_down_1 = this.add.tween(this.girl).to( { y: '+4' }, 400, Phaser.Easing.Cubic.Out);
+		var body_down_2 = this.add.tween(this.girl).to( { y: '+8' }, 400, Phaser.Easing.Cubic.Out);
 		body_down_1.chain(body_down_2);
 		// Move up tween
-		var body_up_1 = this.add.tween(this.girl).to( { y: '-5' }, 500, Phaser.Easing.Cubic.Out);
-		var body_up_2 = this.add.tween(this.girl).to( { y: '-10' }, 500, Phaser.Easing.Cubic.Out);
+		var body_up_1 = this.add.tween(this.girl).to( { y: '-4' }, 400, Phaser.Easing.Cubic.Out);
+		var body_up_2 = this.add.tween(this.girl).to( { y: '-8' }, 400, Phaser.Easing.Cubic.Out);
 		body_up_1.chain(body_up_2);
 		body_down_2.chain(body_up_1);
 		// Start walk animation
@@ -42,26 +73,11 @@ Game.Gameplay.prototype =
 		// tween_head.yoyo(true, 0);
 	},
 
-	playWalkTween: function()
-	{
-		this.walk_tween.start();
-	},
-
-	removeHair: function(sprite, pointer)
-	{
-		sprite.frame = Math.max(sprite.frame - 1, 0);
-	},
-
-	growHair: function()
-	{
-		var sprite = this.girl.getChildAt(1);
-		sprite.frame = Math.min(sprite.frame + 1, sprite.animations.frameTotal - 1);
-	},
-
 	create: function()
 	{
 		var back = this.add.sprite(0, 0, 'background');
 
+		this.crits = this.add.group();
 		this.createGirl();
 		this.pointer = this.add.sprite(0, 0, 'tweezers', 0);
 
@@ -70,6 +86,66 @@ Game.Gameplay.prototype =
 
 		// Hair growth timer
 		this.timer = this.time.events.loop(Phaser.Timer.SECOND * this.growth_interval, this.growHair, this);
+		this.time.events.loop(Phaser.Timer.SECOND * 3, this.createCrit, this);
+
+		// Fade in
+		this.camera.flash('#000000');
+	},
+
+	playWalkTween: function()
+	{
+		this.walk_tween.start();
+	},
+
+	removeHair: function(sprite, pointer)
+	{
+		var child_index = this.girl.getChildIndex(sprite);
+		sprite.frame = Math.max(sprite.frame - 1, 0);
+		var found = this.sprites_to_grow.find(function(value) { return value === child_index; });
+		if (!found)
+		{
+			this.sprites_to_grow.push(child_index);
+		}
+	},
+
+	growHair: function()
+	{
+		//console.log(this.sprites_to_grow);
+		// Rand select in this.sprites_to_grow (the only hair sprites available)
+		if (this.sprites_to_grow.length === 0)
+			return;
+		var index = Game.rand(0, this.sprites_to_grow.length - 1);
+		var child_index = this.sprites_to_grow[index];
+
+		// Increase frame number
+		var sprite = this.girl.getChildAt(child_index);
+		sprite.frame = Math.min(sprite.frame + 1, sprite.animations.frameTotal - 1);
+
+		// Remove sprite from list if it reached its maximum
+		if (sprite.frame === sprite.animations.frameTotal - 1)
+		{
+			this.sprites_to_grow.splice(index, 1);
+		}
+	},
+
+	createCrit: function()
+	{
+		var index = Game.rand(1, 3);
+		var name = 'cr_0' + index + '_body';
+		var x = Game.WIDTH;
+		var y = Game.rand(crit.height*2, Game.HEIGHT);
+		var crit = this.crits.create(x, y, name);
+		crit.addChild(this.make.sprite(0, 0, 'cr_0' + index + '_face'));
+		// Move up tween
+		var show = this.add.tween(crit).to( { x: '-'+(crit.width-80), y: '-'+crit.height }, 240, Phaser.Easing.Linear.Out);
+		show.start();
+		// Z-sorting of crits group
+		this.crits.sort('y', Phaser.Group.SORT_ASCENDING);
+	},
+
+	destroyCrit: function()
+	{
+
 	},
 
 	update: function()
--- js/Preloader.js
+++ js/Preloader.js
@@ -11,16 +11,33 @@ Game.Preloader.prototype =
 		// this.preloadBar = this.add.sprite((Game.GAME_WIDTH-311)/2,
 		//     (Game.GAME_HEIGHT-27)/2, 'preloaderBar');
 		// this.load.setPreloadSprite(this.preloadBar);
- 
-		this.load.image('background', 'assets/image/fondo.png');
- 
-		this.load.spritesheet('dummy', 'assets/image/dummy.png', 27, 40);
-		this.load.spritesheet('body', 'assets/image/corpo.png', 385, 450);
-		this.load.spritesheet('head', 'assets/image/cabeza.png', 385, 450);
-		this.load.spritesheet('face', 'assets/image/cara.png', 385, 450);
-		this.load.spritesheet('tweezers', 'assets/image/pinza.png', 182, 105, 2);
-		this.load.spritesheet('arm_left', 'assets/image/br_es.png', 108, 60, 4);
 
+		this.load.image('background', 'assets/image/fondo.png');
+
+		// Girl
+		this.load.spritesheet('body', 'assets/image/corpo.png', 385, 473);
+		this.load.spritesheet('head', 'assets/image/cabeza.png', 384, 473, 2);
+		this.load.spritesheet('face', 'assets/image/cara.png', 385, 473);
+		// Hair
+		this.load.spritesheet('arm_left', 'assets/image/pelo/br_es.png', 108, 60, 4);
+		this.load.spritesheet('arm_right', 'assets/image/pelo/br_der.png', 45, 77, 4);
+		this.load.spritesheet('armpit_left', 'assets/image/pelo/ax_es.png', 34, 39, 3);
+		this.load.spritesheet('armpit_right', 'assets/image/pelo/ax_der.png', 27, 27, 3);
+		this.load.spritesheet('eyebrow', 'assets/image/pelo/entre.png', 19, 13, 3);
+		this.load.spritesheet('moustache', 'assets/image/pelo/big.png', 33, 11, 3);
+		this.load.spritesheet('pubis', 'assets/image/pelo/pub.png', 78, 17, 3);
+		// Pointer
+		this.load.spritesheet('tweezers', 'assets/image/pinza.png', 182, 105, 2);
+
+		// People
+		this.load.spritesheet('cr_01_body', 'assets/image/criticons/cr_01/cr_01_corpo.png', 382, 332);
+		this.load.spritesheet('cr_01_face', 'assets/image/criticons/cr_01/cr_01_enfado.png', 382, 332);
+		this.load.spritesheet('cr_02_body', 'assets/image/criticons/cr_02/cr_02_corpo.png', 382, 317);
+		this.load.spritesheet('cr_02_face', 'assets/image/criticons/cr_02/cr_02_enfado.png', 382, 317);
+		this.load.spritesheet('cr_03_body', 'assets/image/criticons/cr_03/cr_03_corpo.png', 388, 355);
+		this.load.spritesheet('cr_03_face', 'assets/image/criticons/cr_03/cr_03_enfado.png', 388, 355);
+
+		// Audio
 		this.load.audio('ambient', 'assets/sound/ambient.ogg');
 	},
 
